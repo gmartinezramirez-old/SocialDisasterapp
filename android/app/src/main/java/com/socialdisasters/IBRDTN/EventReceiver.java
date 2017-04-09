5edd03cb800dc8cdd9862c8452a33411b37d2da1
patricio jara <patricio.jarah@usach.cl>
2017-04-09 03:08:40 -0300

    fix

--- android/app/src/main/java/com/socialdisasters/IBRDTN/EventReceiver.java
+++ android/app/src/main/java/com/socialdisasters/IBRDTN/EventReceiver.java
@@ -3,8 +3,6 @@ package com.socialdisasters.IBRDTN;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 
